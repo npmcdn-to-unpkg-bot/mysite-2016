2071629c65360cb77a648d702f69f5a580d754de
Anton Furuholm <anton.furuholm@gmail.com>
2016-03-17 10:29:44 +0100

    Restored changed subnav

--- src/assets/javascript/header.js
+++ src/assets/javascript/header.js
@@ -1,23 +1,20 @@
 (function header() {
 
-    var max_scroll = 50;
+    var max_scroll = 50,
+        subnav = document.querySelector('.sub-nav');
 
-    // var subnav = _.get(".sub-nav")[0];
-    var subnav = document.querySelector(".sub-nav");
-
-    window.addEventListener("scroll", fixNav);
+    window.addEventListener('scroll', fixNav);
 
     function fixNav() {
 
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 
-        if(scrollTop > max_scroll && !subnav.classList.contains(".filterbuttonFixed")) {
-                subnav.classList.add("filterbuttonFixed");
-                // console.log("go floated");
-        }
-        else if(scrollTop < max_scroll && subnav.classList.contains(".filterbuttonFixed") ) {
-                // console.log("return to normal");
-                subnav.classList.remove("filterbuttonFixed");
+        if (scrollTop > max_scroll && !subnav.classList.contains('.filterbuttonFixed')) {
+            subnav.classList.add('filterbuttonFixed');
+
+        } else if (scrollTop < max_scroll && subnav.classList.contains('.filterbuttonFixed')) {
+            subnav.classList.remove('filterbuttonFixed');
+
         }
     }
 
