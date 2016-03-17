817f4b32a8c4d3847267609e0b013e939e3fa904
Anton Furuholm <anton.furuholm@gmail.com>
2016-03-17 14:53:20 +0100

    work

--- src/assets/javascript/header.js
+++ src/assets/javascript/header.js
@@ -1,21 +1,5 @@
 (function header() {
 
-    var max_scroll = 50,
-        subnav = document.querySelector('.sub-nav');
 
-    window.addEventListener('scroll', fixNav);
-
-    function fixNav() {
-
-        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-        if (scrollTop > max_scroll && !subnav.classList.contains('.filterbuttonFixed')) {
-            subnav.classList.add('filterbuttonFixed');
-
-        } else if (scrollTop < max_scroll && subnav.classList.contains('.filterbuttonFixed')) {
-            subnav.classList.remove('filterbuttonFixed');
-
-        }
-    }
 
 })();
--- src/assets/javascript/subnav.js
+++ src/assets/javascript/subnav.js
@@ -1,27 +1,56 @@
+(function subNav() {
 
-if(document.querySelector(".sub-nav") instanceof HTMLElement) {
-    (function subNav() {
+	var max_scroll = 50,
+		subnav = document.querySelector('.sub-nav');
 
-        var max_scroll = 50,
-            subnav = document.querySelector(".sub-nav");
+	if (document.querySelector('.sub-nav') instanceof HTMLElement) {
+	    window.addEventListener('scroll', onScroll);
 
-        // window.addEventListener("scroll", function() {
-        //     onScroll(fixNav);
-        // });
-        window.addEventListener("scroll", fixNav);
+	}
 
-        function fixNav() {
+	function fixNav() {
 
-            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 
-            if(scrollTop > max_scroll && !subnav.classList.contains("subNavFixed")) {
-                    subnav.classList.add("subNavFixed");
+	    if (scrollTop > max_scroll && !subnav.classList.contains('subNavFixed')) {
+	        subnav.classList.add('subNavFixed');
 
-            } else if(scrollTop < max_scroll && subnav.classList.contains("subNavFixed") ) {
-                    subnav.classList.remove("subNavFixed");
-            }
-        }
+	    } else if (scrollTop < max_scroll && subnav.classList.contains('subNavFixed')) {
+	        subnav.classList.remove('subNavFixed');
 
-    })();
-}
+	    }
+	}
+
+	var latestKnownScrollY = 0,
+		ticking = false;
+
+	function onScroll() {
+		latestKnownScrollY = window.scrollY;
+		requestTick();
+	}
+
+	function requestTick() {
+		if(!ticking) {
+			requestAnimationFrame(update);
+		}
+		ticking = true;
+	}
+
+	function update() {
+		// reset the tick so we can
+		// capture the next onScroll
+		ticking = false;
+
+		var currentScrollY = latestKnownScrollY;
+
+		fixNav();
+
+		// read offset of DOM elements
+		// and compare to the currentScrollY value
+		// then apply some CSS classes
+		// to the visible items
+	}
+
+
+})();
 
